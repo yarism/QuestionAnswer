9cba083899c5f9e8033aa623c0393b4cc949f298
yarism <joakim.j.johansson@teliasonera.com>
2015-07-18 23:12:23 +0200

    Added QAService

---
+++ QAService.js
@@ -0,0 +1,43 @@
+(function () {
+    'use strict';
+
+    angular.module('answerApp')
+
+    .factory('QAService', ['$http', '$q', function ($http, $q) {
+
+        return {
+            getQuestion: getQuestion,
+            submitAnswer: submitAnswer
+        };
+
+        function getQuestion() {
+            var def = $q.defer();
+            $http.get("http://www.rly.nu/projects/gp/?action=getQuestion")
+                .success(function(data) {
+                    def.resolve(data);
+                })
+                .error(function() {
+                    def.reject("Failed to get question");
+                });
+            return def.promise;
+        }
+
+        function submitAnswer(id, answer) {
+            var def = $q.defer();
+            $http({
+                    method: 'POST',
+                    url: 'http://www.rly.nu/projects/gp/?action=submitAnswer',
+                    data: "qid=" + id + "&answer=" + answer,
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                })
+                .success(function(data) {
+                    def.resolve(data);
+                })
+                .error(function() {
+                    def.reject("Failed to submit answer");
+                });
+            return def.promise;
+        }
+
+    }]);
+})();
--- mainController.js
+++ mainController.js
@@ -1,5 +1,27 @@
-app.controller("mainController", function($scope) {
-    console.log("hello");
-    $scope.question = "How to tie a tie?";
-    $scope.maxLength = 130 - $scope.question.length;
+app.controller("mainController", function($scope, QAService) {
+    $scope.loadingApp = true;
+
+    initialize();
+
+    $scope.submitAnswer = function(answer) {
+        console.log("scope: ", $scope);
+        console.log($scope.questionId);
+        $scope.sending = true;
+        return QAService.submitAnswer($scope.questionId, answer).then(function() {
+            $scope.sending = false;
+        });
+    };
+
+    function getQuestion() {
+        return QAService.getQuestion().then(function(question) {
+            $scope.question = question[0].question;
+            $scope.maxLength = 130 - question[0].length;
+            $scope.questionId = question[0].id;
+            $scope.loadingApp = false;
+        });
+    }
+
+    function initialize() {
+        getQuestion();
+    }
 });
